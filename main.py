1b74c79f029e90c7922f073560711f065f8d98cf
Nate Dixon <retr0@Nates-MacBook-Air.local>
2024-09-23 20:05:45 -0400

    printing dialogue < 300 char

--- main.py
+++ main.py
@@ -1,2 +1,37 @@
 from bs4 import BeautifulSoup
 
+def get_pairs(paragraphs):
+
+    pairs = {
+        "test" : "test response"
+    }
+
+    for index, p in enumerate(paragraphs):
+
+        dialogue1 = p.get_text()
+        dialogue2 = ""
+
+
+        if "\"" in dialogue1 and len(dialogue1) < 300:
+            pairs.update({p.get_text(): ""})
+
+    return pairs
+
+
+def main():
+
+    filename = "sources/Victoria _ Project Gutenberg.html";
+
+    with open(filename, 'r') as file:
+        content = file.read()
+
+    soup = BeautifulSoup(content, 'html.parser')
+
+    paragraphs = soup.find_all('p')
+
+    pairs = get_pairs(paragraphs)
+
+    for key in pairs:
+        print(key, pairs[key])
+
+main()
