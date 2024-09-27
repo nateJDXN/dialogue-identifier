61b98ac43c6fea38766463b06cab5ca333984885
Nate Dixon <retr0@Nates-MacBook-Air.local>
2024-09-27 07:53:11 -0400

    updated dialogue criteria

--- main.py
+++ main.py
@@ -2,24 +2,28 @@ from bs4 import BeautifulSoup
 
 def get_pairs(paragraphs):
 
-    pairs = {
-        "test" : "test response"
-    }
+    #create pairs dict
+    pairs = {"default" : "default response"}
 
     for index, p in enumerate(paragraphs):
-
+        # key dialogue
         dialogue1 = p.get_text()
-        dialogue2 = ""
 
-
-        if "\"" in dialogue1 and len(dialogue1) < 300:
-            pairs.update({p.get_text(): ""})
+        # check for out of boundsd
+        if index + 1 < len(paragraphs):
+            dialogue2 = paragraphs[index + 1].get_text()    # set value dialogue (response)
+        else:
+            continue
+        
+        # checks for quotatios and max length
+        if dialogue1.count("\"") == 2 and len(dialogue1) < 300 and dialogue2.count("\"") == 2 and len(dialogue2) < 300:
+            pairs.update({dialogue1 : dialogue2})
 
     return pairs
 
 
 def main():
-
+    # define target novel
     filename = "sources/Victoria _ Project Gutenberg.html";
 
     with open(filename, 'r') as file:
